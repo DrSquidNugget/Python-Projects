3db3241b233aac524cfd68e5b4b5db94ccc79d10
DrSquidNugget <56265761+DrSquidNugget@users.noreply.github.com>
2019-10-08 20:11:57 +0100

    Update rockpapersissors.py
    Added detail on RuleMatrix meaning

--- rockpapersissors.py
+++ rockpapersissors.py
@@ -3,6 +3,14 @@
 # Author:  DrSquidNugget
 # Date:    01/02/2019
 #
+# RuleMatrix is based on player 1 at the top and player 2 at the side.
+# The number represents which player wins with 0 for a draw.
+#
+#           Rock    Paper   Scissors
+# Rock      0       2       2
+# Paper     2       0       1
+# Scissors  1       2       0
+#
 import random
 
 rulematrix = [[0,2,2],[2,0,1],[1,2,0]]
